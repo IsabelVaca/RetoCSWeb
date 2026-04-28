4d7f95b1e8d2854a033ac7715bdd3f79dbe18f94
Isabel Vaca <A01352522@tec.mx>
2026-04-27 23:13:12 -0600

    Ranking- Se crearon modelos, funcionalidad en controller, barra búsqueda
    Se crearon los modelos de usuarios, se agregó la funcionalidad de desplegar usuarios ordenados en la lista del ranking, se muestra usuario actual, barra de búsqueda funcional.

---
+++ CSweb/Models/RankingViewModel.cs
@@ -0,0 +1,7 @@
+namespace CSweb.Models;
+
+public class RankingViewModel
+{
+    public List<UsuarioRankingViewModel> Usuarios { get; set; }
+    public UsuarioRankingViewModel UsuarioActual { get; set; }
+}
---
+++ CSweb/Models/UsuarioRankingViewModel.cs
@@ -0,0 +1,12 @@
+namespace CSweb.Models
+
+{
+    public class UsuarioRankingViewModel
+    {
+        public int Id { get; set; } 
+        public int Puntos { get; set; }
+        public int Posicion { get; set; }
+        public string userName { get; set; }
+
+    }
+}
