4fd9a6f936985a72c5bb2fd2e3073fc108f4719d
samanthaacarmona <A00841258@tec.mx>
2026-05-26 08:58:30 -0600

    draft

---
+++ CSweb/Models/HomeCreadorViewModel.cs
@@ -0,0 +1,10 @@
+namespace CSweb.Models
+{
+    public class HomeCreadorViewModel
+    {
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public string Imagen { get; set; }
+        public string Seguidores { get; set; }
+    }
+}
---
+++ CSweb/Models/HomeTendenciaViewModel.cs
@@ -0,0 +1,8 @@
+namespace CSweb.Models
+{
+    public class HomeTendenciaViewModel
+    {
+        public string Nombre { get; set; }
+        public int Posts { get; set; }
+    }
+}
