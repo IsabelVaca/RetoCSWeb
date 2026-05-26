cca107aade537171166d96a560c5234e1ee41fcf
Blackout342 <jhernandezgonzalez253@gmail.com>
2026-05-25 22:28:17 -0600

    Push API pepe main

---
+++ CSweb/Models/UsuarioNavbarViewModel.cs
@@ -0,0 +1,8 @@
+namespace CSweb.Models;
+
+// Datos del usuario en la pastilla del header (_Layout). Sin valores por defecto.
+public class UsuarioNavbarViewModel
+{
+    public string? UserName { get; set; }
+    public string? Iniciales { get; set; }
+}
